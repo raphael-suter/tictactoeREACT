1f1c0ac22492484c1d369ca7036e0f635f2c82b1
Raphael <raphael.suter@axa.ch>
2020-06-16 16:26:18 +0200

    Add versioning

--- src/components/Dialog/AXAToggleSwitch.tsx
+++ src/components/Dialog/AXAToggleSwitch.tsx
@@ -1,6 +1,6 @@
 import createAXAToggleSwitchReact from "@axa-ch/toggle-switch/lib/index.react";
 import { createElement } from "react";
 
-const AXAToggleSwitch = createAXAToggleSwitchReact(createElement);
+const AXAToggleSwitch = createAXAToggleSwitchReact(createElement, "ttt");
 
 export default AXAToggleSwitch;
--- src/components/Dialog/AXAbutton.tsx
+++ src/components/Dialog/AXAbutton.tsx
@@ -1,8 +1,8 @@
-import createAXAButtonReact from '@axa-ch/button/lib/index.react';
-import { createElement } from 'react';
-import styled from 'styled-components';
+import createAXAButtonReact from "@axa-ch/button/lib/index.react";
+import { createElement } from "react";
+import styled from "styled-components";
 
-const AXAbutton = styled(createAXAButtonReact(createElement))`
+const AXAbutton = styled(createAXAButtonReact(createElement, "ttt"))`
   margin-top: 0.6rem;
 `;
 
