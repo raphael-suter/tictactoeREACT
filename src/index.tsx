592f6022dceea19c78ac30604c6baeeb0593cecc
Raphael <raphael.suter@axa.ch>
2020-07-06 14:08:48 +0200

    Modify dev server

--- src/index.tsx
+++ src/index.tsx
@@ -1,3 +1,4 @@
+import '@axa-ch/patterns-library-polyfill';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Page from './components/Page';
--- webpack.config.js
+++ webpack.config.js
@@ -1,43 +1,42 @@
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const { CleanWebpackPlugin } = require('clean-webpack-plugin');
-const path = require('path');
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const path = require("path");
 
 module.exports = {
-  entry: './src/index.tsx',
+  entry: "./src/index.tsx",
   module: {
     rules: [
       {
         test: /\.tsx$/,
-        use: 'ts-loader',
-        exclude: /node_modules/
+        use: "ts-loader",
+        exclude: /node_modules/,
       },
       {
         test: /\.(scss|css)$/,
-        use: [
-          'style-loader',
-          'css-loader',
-          'sass-loader'
-        ]
+        use: ["style-loader", "css-loader", "sass-loader"],
       },
       {
         test: /\.(woff|woff2|eot|ttf|png|svg|jpg|gif)$/,
-        use: [
-          'file-loader'
-        ]
-      }
-    ]
+        use: ["file-loader"],
+      },
+    ],
   },
   resolve: {
-    extensions: ['.ts', '.tsx', '.js']
+    extensions: [".ts", ".tsx", ".js"],
   },
   output: {
-    filename: 'bundle.[hash].js',
-    path: path.resolve(__dirname, 'build')
+    filename: "bundle.[hash].js",
+    path: path.resolve(__dirname, "build"),
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: './public/index.html'
+      template: "./public/index.html",
     }),
-    new CleanWebpackPlugin()
-  ]
-}
+    new CleanWebpackPlugin(),
+  ],
+  devServer: {
+    host: "192.168.178.52",
+    port: 8081,
+    disableHostCheck: true,
+  },
+};
